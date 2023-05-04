533c6c64b27cda3e2e61c7ce152ff6a7a737d689
Ismael Luna <ismaelunalvarez@gmail.com>
2023-05-04 15:42:48 +0200

    Puesta al dia

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [joke, setJoke] = useState("There's still no jokes");
+  const refClick = useRef();
 
-  function nextjoke() {
+  const exChange = () => {
+    // Whenever a reference is used, the "current" must always be indicated.
+    refBox.current.innerHTML = Number(refBox.current.innerHTML) * change;
+  };
+  
+  const nextjoke = () => {
     useEffect(() => {
       const url = "https://api.chucknorris.io/jokes/random/";
-      const peticion = fetch(url);
-
-      peticion.then
+      const peticion = fetch(url)
       peticion
         .then((datos) => datos.json())
         .then((lectura) => {
           setJoke(lectura.value);
-        });
-    });
+        })
+        .catch(() => {
+          console.log("Error de lectura");
+        })
+    }, []);
   }
 
   return (
     <>
-      <div></div>
-      <button onChange={nextjoke} value={joke}>
+      <div>{joke}</div>
+      <button ref={refClick} onChange={nextjoke}>
         Next joke!
       </button>
 
